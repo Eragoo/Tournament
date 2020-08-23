d7f060913735d8a52fd63b48b54fbf15258a9d38
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-08-23 18:48:20 +0300

    participant entity created; added mapstruct;

---
+++ src/main/java/com/Eragoo/Tournament/participant/ParticipantController.java
@@ -0,0 +1,9 @@
+package com.Eragoo.Tournament.participant;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/participant")
+public class ParticipantController {
+}
