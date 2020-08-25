889e85c920ada0c444a3fccd4701ff12b546ee74
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-08-25 20:01:42 +0300

    removed unused import

--- src/main/java/com/Eragoo/Tournament/tournament/match/MatchResults.java
+++ src/main/java/com/Eragoo/Tournament/tournament/match/MatchResults.java
@@ -3,7 +3,6 @@ package com.Eragoo.Tournament.tournament.match;
 import com.Eragoo.Tournament.participant.ParticipantDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @AllArgsConstructor
 @Getter
